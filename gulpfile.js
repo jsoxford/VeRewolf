1d9ead5b717a6e1716f1cdf71ac52218f89428da
Max Glenister <moglenstar@gmail.com>
2016-07-23 11:54:03 +0100

    basic gulp/pug setup

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+var paths = {
+  src: './src/**',
+  dist: './public/'
+}
+
+gulp.task('build', function () {
+  gulp.src('')
+});
+gulp.task('default', ['build']);
